9fb0395eb2a5de8a3546dcd41d5e59c2ef8dbab2
Carla Aluvai <carngie@gmail.com>
2022-10-07 14:48:57 +0300

    Created Unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    public float climbForce = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+
+        
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerRb.velocity = new Vector3(transform.position.x, climbForce, transform.position.z);
+        }
+    }
+}
