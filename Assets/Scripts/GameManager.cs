c161f4e03e4886a453b394226f8482fbaef1b520
Carla Aluvai <carngie@gmail.com>
2022-10-09 14:28:15 +0300

    Implemented the timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static bool gameiIsPaused;
+    public Button restartButton;
+    public TextMeshProUGUI timerText;
+    private float gameTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameTime = 30;
+        timerText.text = "Time: " + gameTime;
+        gameiIsPaused = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        gameTime -= Time.deltaTime;
+        if (gameTime <= 0)
+        {
+            timerText.text = "Time Over!";
+
+
+        }
+        else
+        {
+
+
+            timerText.gameObject.SetActive(true);
+            timerText.text = "Time: " + Mathf.FloorToInt(gameTime % 60);
+
+        }
+
+
+    }
+
+
+    public void RestartGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+
+
+
     }
 }
